a9c9e6a1b3b42d2e5f58af05968623453880daf8
Beksultan Turganbaev <beksultan.turganbaev.kk@gmail.com>
2024-02-15 07:51:57 +0300

    Second part actions.js add find(selector),closest(selector), siblings()

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,12 @@ $('button').on('click', function() {
     $('div').eq(1).toggleClass('active');
 });
 
+$('div').click(function() {
+    console.log($(this).index());
+});
+
+// console.log($('div').eq(2).find('.some'));
+// console.log($('.some').closest('.findmea'));
+console.log($('.findme').siblings());
+
 // console.log($('button').html('hello'));
